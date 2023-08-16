8f646d8bb9ff786e225f4c359800a5d720e648b9
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2023-08-16 11:02:51 +0100

    randoms react work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, countStart] = useState(0);
+  const handle = () => {
+    return countStart(count + 1);
+  };
+  const handlenegative = () => {
+    return countStart(count - 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="flex flex-col gap-9 text-blue-800 w-full h-screen justify-center items-center bg-gray-700">
+      <p className="text-6xl text-white">{count}</p>
+      <div className="gap-3 flex">
+        <button
+          className="bg-blue-500 rounded shadow-lg p-2"
+          onClick={handlenegative}
         >
-          Learn React
-        </a>
-      </header>
+          Decrease
+        </button>{" "}
+        <button className="bg-blue-500 rounded shadow-lg p-2" onClick={handle}>
+          Increase
+        </button>
+      </div>
     </div>
   );
 }
-
 export default App;
